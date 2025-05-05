d985d6eb95af7c63b717e232f10e0ac19194841e
Stevierajesh <78890359+Stevierajesh@users.noreply.github.com>
2025-05-05 15:10:49 -0400

    Alpha 1.6B
    Trying again with the maintenence fix:

--- maintain.js
+++ maintain.js
@@ -4,7 +4,7 @@ let shouldRedirect = true;
 redirectToMaintain(shouldRedirect);
 
 function redirectToMaintain(isTrue) {
-    if (isTrue) {
+    if (isTrue && !currentPage.includes("maintain.html")) {
         window.location.href = "maintain.html";
     }
 }
